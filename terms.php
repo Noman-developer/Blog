c37e8d5e87d665354c088f741ed976ea99537b72
Noman Javed <noman19304@gmail.com>
2025-11-28 13:08:00 +0500

    Update terms.php

--- terms.php
+++ terms.php
@@ -11,24 +11,24 @@
     <p>
         It is your responsibility to review these Terms of Use periodically. If at any time you find these Terms of Use unacceptable or if you do not agree to these Terms of Use, please do not use this Site or any Applications. We may revise these Terms of Use at any time without notice to you.
     </p>
-    <ol class="container ml-3">
-        <h2><li>Privacy Policy</li></h2>
-        <p>
-            eBlogs respects your privacy and permits you to control the treatment of your personal information. A complete statement of current Privacy Policy can be found by <u><a href="privacy.php" class="text-info">Clicking here</a></u>. eBlog's Privacy Policy is expressly incorporated into this Agreement by reference
-        </p>
-        <h2><li>Ownership</li></h2>
-        <p>
-            This Website belongs to eBlogs. All rights, Titles, Designs, Logos and Images are reserved only for eBlogs and can be copied of used after written permission of Owner.
-        </p>
-        <h2><li>Links to Third Party Sites</li></h2>
-        <p>
-            This website has not given access to any third party.
-        </p>
-        <h2><li>Data Sharing</li></h2>
-        <p>
-            No data of customers is shared by any party except eBlog's Internal And Operational team.
-        </p>
-    </ol>
+                <ol class="container ml-3">
+                    <h2><li>Privacy Policy</li></h2>
+                    <p>
+                        eBlogs respects your privacy and permits you to control the treatment of your personal information. A complete statement of current Privacy Policy can be found by <u><a href="privacy.php" class="text-info">Clicking here</a></u>. eBlog's Privacy Policy is expressly incorporated into this Agreement by reference
+                    </p>
+                    <h2><li>Ownership</li></h2>
+                    <p>
+                        This Website belongs to eBlogs. All rights, Titles, Designs, Logos and Images are reserved only for eBlogs and can be copied of used after written permission of Owner.
+                    </p>
+                    <h2><li>Links to Third Party Sites</li></h2>
+                    <p>
+                        This website has not given access to any third party.
+                    </p>
+                    <h2><li>Data Sharing</li></h2>
+                    <p>
+                        No data of customers is shared by any party except eBlog's Internal And Operational team.
+                    </p>
+                </ol>
         </div>
     </div>
 
@@ -38,3 +38,4 @@
 
 <?php include'headers/copyright.php' ?>
 <?php include'headers/footer.php' ?>
+
